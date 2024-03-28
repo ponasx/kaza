650974779390a6f1c0a5b766364ef1673ddeb27a
gooday <112113841+vesen01@users.noreply.github.com>
2024-03-28 17:14:36 +0400

    _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
-const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou', 'edgetunnel.anycast.eu.org''time.cloudflare.com', 
+const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou', 'edgetunnel.anycast.eu.org','time.cloudflare.com', 
 	'shopify.com',
 	'time.is',
 	'icook.hk',
@@ -39,8 +39,6 @@ const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.c
 	'www.udemy.com',
 	'www.baipiao.eu.org',
 	'cdn.anycast.eu.org',
-	'cdn-all.xn--b6gac.eu.org',
-	'cdn-b100.xn--b6gac.eu.org',
 	'xn--b6gac.eu.org',
 	'edgetunnel.anycast.eu.org',
 	'alejandracaiccedo.com',
